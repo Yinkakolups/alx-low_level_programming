81022e119b94243f30741359956a5dc15ef96d93
yinkakolups <olayinkakolupo@gmail.com>
2022-12-10 13:17:43 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 
 /**
-
-* main - prints the phrase "Programming is like building a multilingual puzzle
-* Return: 0 if exited properly , non-zero otherwise
-* 	*/
-int min(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		puts(\ "Programming is like building a multilingual puzzle");
-			return (0);
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
