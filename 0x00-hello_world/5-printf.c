d47f08891910228e31058f5cdc4847c074935885
yinkakolups <olayinkakolupo@gmail.com>
2022-12-10 13:54:18 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%", "with proper grammar, but the outcome is a pieceof art,\n");
+	return (0)
+}
