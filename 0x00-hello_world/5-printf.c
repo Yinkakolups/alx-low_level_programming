e8cd25ab2c3c86b425d6f67139b195a1620bec13
yinkakolups <olayinkakolupo@gmail.com>
2022-12-10 15:12:27 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
