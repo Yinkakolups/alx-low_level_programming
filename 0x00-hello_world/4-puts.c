a97ef1334ad4b2e35fe6244847fd17d4bafbe5bd
yinkakolups <olayinkakolupo@gmail.com>
2022-12-10 13:46:59 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
