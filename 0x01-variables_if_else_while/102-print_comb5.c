0701c476ec75b43caa2f369cbade0bcbdfade518
yinkakolups <olayinkakolupo@gmail.com>
2022-12-11 09:52:45 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,7 +25,7 @@ int main(void)
 					putchar(k);
 					putchar(j);
 
-					if (c + i + k + j ==227 && c == 57)
+					if (c + i + k + j == 227 && c == 57)
 					{
 					break;
 					}
@@ -41,6 +41,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-						
-				
+}				
