9f59c11f28ef0c6c3bce6e058dbd636067e032fe
yinkakolups <olayinkakolupo@gmail.com>
2022-12-11 09:09:15 -0800

    advanced task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints 3 digits number combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, i, k;
+
+	for (c ='0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
