27fe7307939626d793d8ef35f42b81da7f368169
yinkakolups <olayinkakolupo@gmail.com>
2022-12-13 11:45:40 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - Prints _putchar as a message.
- *
+ * main - Prints _putchar
+ *is written by yinka
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
