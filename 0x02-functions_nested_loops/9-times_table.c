24d0ef41342065cda8deeba6dba13c7cf15507b4
yinkakolups <olayinkakolupo@gmail.com>
2022-12-16 06:15:09 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,24 +15,24 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
-_putchar(k + '0');
+				_putchar(k + '0');
 			}
 
 			if (k < 10 && j != 0)
 			{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
