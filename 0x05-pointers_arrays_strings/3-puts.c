24009701ee483f3a0abb381875042854b72b1dd2
yinkakolups <olayinkakolupo@gmail.com>
2022-12-19 09:11:31 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts - prints a string followed by a new line, to stdout
+ * _puts - prints a string followed by a new line, to stdout
  * @str: string to print
  */
 void _puts(char *str)
