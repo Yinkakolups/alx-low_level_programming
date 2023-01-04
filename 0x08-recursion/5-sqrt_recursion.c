3e448ef53012922a5bf8a18b5c602ff8bb4049b2
yinkakolups <olayinkakolupo@gmail.com>
2023-01-04 07:38:21 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,23 +7,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (_sqrt(n, 1));
 }
+
 /**
- * actual_sqrt_recursion - recurses to find the natural
+ * _sqrt_recursion - recurses to find the natural
  * square root of a number
  * @n: number to calculate the square root of
  * @i: iterator
  *
  * Return: the resulting square root
  */
-int actual_sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n, int i)
 {
-	if (i * i > n)
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
