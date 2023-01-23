a536e46b4a0c8e7ccf5bc51db0866533251d21de
yinkakolups <olayinkakolupo@gmail.com>
2023-01-23 00:30:31 -0800

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,11 +17,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(valist, char *)
+		str = va_arg(valist, char *);
 
 		if (str)
 			printf("%s", str);
-		else
+		if else
 			printf("(nil)");
 
 		if (i < n - 1)
